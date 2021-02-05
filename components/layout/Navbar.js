a8edc94bb4beddf323c0907ff278727346f0fd16
Prashant <prashantamoli@outlook.com>
2021-02-05 22:35:50 +0530

    ➕Layout

---
+++ components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/layout/layout.module.css'
+
+const Navbar = () => {
+	return (
+		<nav className={styles.navbar}>
+			<img src="../../public/favicon.ico" alt="ICON" />
+
+			<ul>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+
+				<Link href="/">
+					<a>Profile</a>
+				</Link>
+
+				<Link href="/">
+					<a>Contact Us</a>
+				</Link>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
